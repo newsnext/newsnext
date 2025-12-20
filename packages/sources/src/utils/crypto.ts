ca67581141dc077665d23099a2cba28887e6353e
ourongxing <orongxing@gmail.com>
2025-12-20 10:01:38 +0000

    feat(sources): replace md5 dependency with @smithy/md5-js and update crypto utility

--- packages/sources/src/utils/crypto.ts
+++ packages/sources/src/utils/crypto.ts
@@ -1,7 +1,19 @@
-import _md5 from "md5"
+import { Md5 } from "@smithy/md5-js"
 
 export async function md5(s: string) {
-  return _md5(s)
+  try {
+    const hash = await myCrypto(s, "MD5")
+    console.log("use crypto", hash)
+    return hash
+  } catch {
+    const hasher = new Md5()
+    hasher.update(s)
+    const result = await hasher.digest()
+    console.log("use md5", result)
+    return Array.from(result)
+      .map(b => b.toString(16).padStart(2, "0"))
+      .join("")
+  }
 }
 
 type Algorithm = "MD5" | "SHA-1" | "SHA-256" | "SHA-384" | "SHA-512"
