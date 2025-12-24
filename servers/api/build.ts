3cab7773a2bc41ec56baf592db3f7cf07de5ba2a
ourongxing <orongxing@gmail.com>
2025-12-24 07:10:11 +0000

    chore: test vercel

--- servers/api/build.ts
+++ servers/api/build.ts
@@ -2,9 +2,21 @@ import { build as tsdownBuild } from "tsdown"
 
 async function build() {
   await tsdownBuild({
-    entry: ["src/index.ts", "src/index.cf.ts"],
+    entry: ["src/index.ts"],
     outDir: "dist",
+    outExtensions: () => ({
+      js: ".js",
+    }),
+    clean: true,
   })
+
+  await Bun.build({
+    entrypoints: ["src/index.bun.ts"],
+    outdir: "dist",
+    target: "bun",
+    bytecode: true,
+  })
+
   await Bun.$`cd ../../apps/extension && bun run build:web --outDir ../../servers/api/public --emptyOutDir`
 }
 
--- servers/api/src/index.bun.ts
+++ servers/api/src/index.bun.ts
@@ -1,8 +1,10 @@
 import type { CacheAdapter } from "@newsnext/cache"
 import { trpcServer } from "@hono/trpc-server"
 import { Hono } from "hono"
+import { serveStatic } from "hono/bun"
 import { cors } from "hono/cors"
 import { logger } from "hono/logger"
+import { CACHE_DB_PATH } from "../../../data"
 import { appRouter } from "./app-router"
 
 export type { AppRouter } from "./app-router"
@@ -20,14 +22,12 @@ app.use("/*", cors())
 
 app.use("/api/trpc/*", async (c, next) => {
   if (!adapter) {
-    if (c.env.CACHE_DB) {
-      try {
-        const { D1CacheAdapter } = await import("@newsnext/cache/d1")
-        adapter = new D1CacheAdapter(c.env.CACHE_DB)
-        console.log("Using D1 cache adapter")
-      } catch (e) {
-        console.error("Failed to initialize D1 cache adapter:", e)
-      }
+    try {
+      const { SqliteCacheAdapter } = await import("@newsnext/cache/sqlite")
+      adapter = new SqliteCacheAdapter(CACHE_DB_PATH)
+      console.log("Using Sqlite cache adapter")
+    } catch (e) {
+      console.error("Failed to initialize Sqlite cache adapter:", e)
     }
     if (!adapter) {
       const { MemoryCacheAdapter } = await import("@newsnext/cache/memory")
@@ -48,6 +48,12 @@ app.use("/api/trpc/*", (c, next) => {
     }),
   })(c, next)
 })
+
+app.get("/*", (c, next) => {
+  return serveStatic({ root: "./public" })(c, next)
+})
+
+app.get("*", serveStatic({ path: "./public/index.html" }))
 
 export default {
   port: process.env.PORT ?? 4000,
