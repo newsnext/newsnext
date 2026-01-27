575f745bd6920c4cf5ed02bdac49464269457c96
ourongxing <orongxing@gmail.com>
2026-01-27 10:58:23 +0000

    ci: test vercel

--- servers/proxy/tsdown.config.ts
+++ servers/proxy/tsdown.config.ts
@@ -2,17 +2,16 @@ import bunAdapter from "@newsnext/build/adapter/bun"
 import cloudflareWorkersAdapter from "@newsnext/build/adapter/cloudflare-workers"
 import vercelAdapter from "@newsnext/build/adapter/vercel"
 import Build from "@newsnext/build/rolldown"
-import { getRuntimeKey } from "hono/adapter"
 import { defineConfig } from "tsdown"
 
 function getAdapter() {
-  const runtimeKey = getRuntimeKey()
-  if (runtimeKey === "edge-light") {
+  console.log(globalThis.navigator?.userAgent)
+  if (process.env.VERCEL) {
     console.log("using vercel adapter")
     return vercelAdapter({
       staticPaths: ["public"],
     })
-  } else if (runtimeKey === "workerd") {
+  } else if (globalThis.navigator?.userAgent === "Cloudflare-Workers") {
     console.log("using cloudflare workers adapter")
     return cloudflareWorkersAdapter({
       staticPaths: ["public"],
