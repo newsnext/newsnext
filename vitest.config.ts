04eb18126a0c724910ae219a73f70b8dd84c76f4
ourongxing <orongxing@gmail.com>
2026-04-11 17:08:59 +0000

    test(config): ignore examples in vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,17 @@ import { defineConfig } from "vitest/config"
 export default defineConfig({
   test: {
     globals: true,
+    include: [
+      "apps/**/*.{test,spec}.{ts,tsx}",
+      "packages/**/*.{test,spec}.{ts,tsx}",
+      "servers/**/*.{test,spec}.{ts,tsx}",
+      "data/**/*.{test,spec}.{ts,tsx}",
+    ],
+    exclude: [
+      "**/node_modules/**",
+      "**/dist/**",
+      "examples/**",
+      "**/examples/**",
+    ],
   },
 })
